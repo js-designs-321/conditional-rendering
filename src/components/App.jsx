52f5d89424201d0542726be22be44b070f081d28
js-designs-321 <jatni.321@gmail.com>
2022-06-20 22:27:17 +0530

    Display page conditionally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import Login from "./Login";
+
+var loggedIn = true; 
+
+function Display(){
+    if(loggedIn){
+      return <h1>Hello</h1>; 
+    }else{
+      return <Login />; 
+    }
+}
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {Display()}
     </div>
   );
 }
